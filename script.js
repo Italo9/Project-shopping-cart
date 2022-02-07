c8db2846b731460ba29fa1004d4731c910bab9a9
ítalo Lima <contatoitallo@hotmail.com>
2022-02-07 11:01:15 -0300

    Soma o valor total dos itens do carrinho de compras-Req5

--- script.js
+++ script.js
@@ -23,24 +23,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui event.target.remove 
-  this.remove();
-  saveCartItems(ol);
-}
-
-async function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  // const ol = document.querySelector('.cart__items');
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  ol.appendChild(li);
-  return li;
-}
-
-// const Pc = await fetchProducts('computador');
-
 async function somatorioItemCarrinho() {
   let cont = 0;
   const li = document.querySelectorAll('.cart__item');
@@ -58,6 +40,25 @@ async function somatorioItemCarrinho() {
   valorPc.innerText = cont;
   console.log(cont);
 }
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui event.target.remove 
+  this.remove();
+  saveCartItems(ol);
+  somatorioItemCarrinho();
+}
+
+async function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  // const ol = document.querySelector('.cart__items');
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  ol.appendChild(li);
+  return li;
+}
+
+// const Pc = await fetchProducts('computador');
 
 async function botaoAddCarinho(event) {
   const arrayBotao = [...document.getElementsByClassName('item__add')];
@@ -81,6 +82,10 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   section.addEventListener('click', botaoAddCarinho);
   return section;
+}
+
+function inicioAlerta() {
+  alert('carregando...');
 }
 
 async function criarLista() {
