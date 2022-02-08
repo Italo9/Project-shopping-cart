4a27ce8a65673ee50011e6ce91fb03227b0ad514
ítalo Lima <contatoitallo@hotmail.com>
2022-02-07 22:27:10 -0300

    Teste da função getSavedCartItems emulando o DOM-Req 11

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,17 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    // simulo o dom criando um elemento que tem a propriedade inenerHtML PARA SOMENTE ASSIM A FUNÇÃO TER ALGUMA COISA PARA PODER ALTERAR tendo em vista que a funçao getSavedCartItems altera o innerHtml na linha 5 quando recebe as informações salvas no localStorage
+    document.body.innerHTML = '<ol class="simulaDom"><li>Item</li></ol>'
+    const simulandoDom = document.querySelector('.simulaDom');
+    getSavedCartItems(simulandoDom);
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro', () => {
+    document.body.innerHTML = '<ol class="simulaDom"><li>Item</li></ol>'
+    const simulandoDom = document.querySelector('.simulaDom');
+    getSavedCartItems(simulandoDom);
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
